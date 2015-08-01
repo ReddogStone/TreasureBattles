ef09ef5502f962d0b447085cdc4dfa4c008552ab
Reddogstone <thereddog@gmx.net>
2015-08-01 08:51:27 +0200

    Better controls

--- engine/vector.js
+++ engine/vector.js
@@ -11,14 +11,25 @@ var Vector = (function() {
 		return make(v.x * s, v.y * s);
 	}
 
+	function sqLength(v) {
+		return v.x * v.x + v.y * v.y;
+	}
+
 	return {
 		make: make,
 		clone: Point.clone,
 		add: add,
 		neg: neg,
 		mul: mul,
+		sub: function(v1, v2) {
+			return make(v1.x - v2.x, v1.y - v2.y);
+		},
 		lerp: function(v1, v2, a) {
 			return add(mul(v1, 1 - a), mul(v2, a));
+		},
+		sqLength: sqLength,
+		length: function(v) {
+			return Math.sqrt(sqLength(v));
 		}
 	}
 })();
--- game/behaviors/root.js
+++ game/behaviors/root.js
@@ -10,9 +10,20 @@ var RootBehavior = (function() {
 			var render = assets.renderScripts[getScriptPath(entity.renderScript)];
 			render && render.default && render.default(context, entity);
 		});
+
+		var player = world.entities.player;
+		context.font = 'normal 1em Trebuchet';
+		context.fillStyle = 'black';
+		context.fillRect(0, 0, 300, 70);
+		context.fillStyle = 'white';
+		context.textAlign = 'left';
+		context.textBaseline = 'top';
+		context.fillText(JSON.stringify(player.direction), 10, 10);
+		context.fillText(JSON.stringify(player.target), 10, 30);
+		context.fillText(JSON.stringify(player.gridPos), 10, 50);
 	}
 
-	var PLAYER_SPEED = 5;
+	var PLAYER_SPEED = 10;
 
 	function update(world, deltaTime) {
 		var player = world.entities.player;
@@ -20,43 +31,68 @@ var RootBehavior = (function() {
 		var tileSize = tileMap.tileSize;
 
 		var direction = player.direction;
-		var from = player.from;
-		var to = player.to;
-		var percentage = 1;
+		var gridPos = player.gridPos;
+		var target = player.target;
 
-		if ((from.x !== to.x) || (from.y !== to.y)) {
-			percentage = player.percentage + PLAYER_SPEED * deltaTime;
+		if (target) {
+			var delta = Vector.sub(target, gridPos);
+			var length = Vector.length(delta);
+
+			if (length <= PLAYER_SPEED * deltaTime) {
+				gridPos = Point.clone(target);
+				target = null;
+				deltaTime -= length / PLAYER_SPEED;
+			} else {
+				gridPos = Vector.add(gridPos, Vector.mul(delta, PLAYER_SPEED * deltaTime / length));
+				deltaTime = 0;
+			}
 		}
 
-		if ( (percentage >= 1) && ((direction.x !== 0) || (direction.y !== 0)) ) {
-			from = Vector.clone(to);
-			to = Vector.add(from, direction);
-			percentage -= 1;
-		}
+		gridPos = Vector.add(gridPos, Vector.mul(direction, PLAYER_SPEED * deltaTime));
 
-		var gridPos = Vector.lerp(from, to, percentage);
-		var pos = [tileMap.pos, Vector.make(gridPos.x * tileSize.x, gridPos.y * tileSize.y), Vector.mul(tileSize, 0.5)].reduce(Vector.add);
+		var pos = [
+			tileMap.pos,
+			Vector.make(gridPos.x * tileSize.x, gridPos.y * tileSize.y),
+			Vector.mul(tileSize, 0.5)
+		].reduce(Vector.add);
+
 		var newPlayer = player.merge({
 			pos: pos,
-			percentage: percentage,
-			from: from,
-			to: to
+			gridPos: gridPos,
+			target: target
 		});
 		return world.with(['entities', 'player'], newPlayer);
 	}
 
 	function keypress(world, keyCode) {
 		var player = world.entities.player;
-		var newDirection = player.direction;
+		var direction = player.direction;
 
 		switch (keyCode) {
-			case 39: newDirection = Vector.make(1, 0); break;
-			case 37: newDirection = Vector.make(-1, 0); break;
-			case 40: newDirection = Vector.make(0, 1); break;
-			case 38: newDirection = Vector.make(0, -1); break;
+			case 39: direction = Vector.make(1, 0); break;
+			case 37: direction = Vector.make(-1, 0); break;
+			case 40: direction = Vector.make(0, 1); break;
+			case 38: direction = Vector.make(0, -1); break;
 		}
 
-		return world.with(['entities', 'player', 'direction'], newDirection);
+		var gridPos = Point.clone(player.gridPos);
+		if ((player.direction.x !== 0) && (direction.x === 0)) {
+			var targetX = (player.direction.x > 0 ? Math.ceil : Math.floor)(gridPos.x);
+			target = Point.make(targetX, gridPos.y);
+		} else if ((player.direction.y !== 0) && (direction.y === 0)) {
+			var targetY = (player.direction.y > 0 ? Math.ceil : Math.floor)(gridPos.y);
+			target = Point.make(gridPos.x, targetY);
+		} else {
+			target = null;
+		}
+
+		var newPlayer = player.merge({
+			gridPos: gridPos,
+			direction: direction,
+			target: target
+		});
+
+		return world.with(['entities', 'player'], newPlayer);
 	}
 
 	function main(world) {
@@ -114,9 +150,8 @@ var RootBehavior = (function() {
 						renderScript: 'tile-map'
 					},
 					player: {
-						from: Point.make(62, 46),
-						to: Point.make(62, 46),
-						percentage: 0,
+						gridPos: Point.make(62, 46),
+						target: null,
 						direction: Point.make(0, 0),
 						size: 16,
 						renderScript: 'player'
